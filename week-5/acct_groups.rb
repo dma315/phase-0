4808b8c0461ad4e1c8db8592c14a0c60a694c158
dma315 <dma315@gmail.com>
2015-11-18 15:20:07 -0600

    Complete acct groups challenge

---
+++ week-5/acct_groups.rb
@@ -0,0 +1,98 @@
+# Create initial array of people (of various sizes)
+people = ["Adell Hanson-Kahn","Alex DeLaPena","Bison Hubert","Caitlyn Yu","Caroline Artz","Celeen Rusk","Gary Sperka","Iulia Shoimaru","James Hwang","John Dees","Katy Duncan","Laura Crowe","Lorena Mesa","Matt Higgins","Michael Hamel","Morgan McCrory","Morgan O'Leary","Neal Peters","Sarah Taurchini","Jack Abernethy","Mohammad Amin","Zollie Barnes","Reuben Brandt","Dana Breen","Breton Burnett","Saundra Vanessa Caneda","Luis De Castro","Nicolette Chambers","Kerry Choy","Nick Davies","KB DiAngelo","Katherine DiAngelo","Adrian Diaz","David Diaz","Bob Dorff","Michael Du","Paul Dynowski","Jenna Espezua","Sean Fleming","Jennifer Gilbert","Marcel Haesok","Albert Hahn","Arthur Head","Jonathan Huang","Thomas Huang","Alex Jamar","Kevin Jones","Steven Jones","Cole Kent","Coleby Kent","Caroline","Caroline Kenworthy","Calvin Lang","Yi Lu","David Ma","Sean Massih","Tom McHenry","Alex Mitzman","Lydia Nash","Brenda Nguyen","Matthew Oppenheimer","Mason Pierce","Joe Plonsker","Mira Scarvalone","Joseph Scott","Chris Shahin","Benjamin Shpringer","Lindsey Stevenson","Phil Thomas","Philip Thomas","Gary Tso","Ting Wang","Monique Williamson","Regina Wong","Hanah Yendler"]
+people1 = ["Adell Hanson-Kahn","Alex DeLaPena","Bison Hubert","Caitlyn Yu","Caroline Artz","Celeen Rusk","Gary Sperka","Iulia Shoimaru","James Hwang","John Dees","Katy Duncan","Laura Crowe","Lorena Mesa","Matt Higgins","Michael Hamel","Morgan McCrory","Morgan O'Leary","Neal Peters","Sarah Taurchini","Jack Abernethy","Mohammad Amin","Zollie Barnes","Reuben Brandt","Dana Breen","Breton Burnett","Saundra Vanessa Caneda","Luis De Castro","Nicolette Chambers","Kerry Choy","Nick Davies","KB DiAngelo","Katherine DiAngelo","Adrian Diaz","David Diaz","Bob Dorff","Michael Du","Paul Dynowski","Jenna Espezua","Sean Fleming","Jennifer Gilbert","Marcel Haesok","Albert Hahn","Arthur Head","Jonathan Huang","Thomas Huang","Alex Jamar","Kevin Jones","Steven Jones","Cole Kent","Coleby Kent","Caroline","Caroline Kenworthy","Calvin Lang","Yi Lu","David Ma","Sean Massih","Tom McHenry","Alex Mitzman","Lydia Nash","Brenda Nguyen","Matthew Oppenheimer","Mason Pierce","Joe Plonsker","Mira Scarvalone","Joseph Scott","Chris Shahin","Benjamin Shpringer","Lindsey Stevenson","Phil Thomas","Philip Thomas","Gary Tso","Ting Wang","Monique Williamson","Regina Wong"]
+people2 = ["Adell Hanson-Kahn","Alex DeLaPena","Bison Hubert","Caitlyn Yu","Caroline Artz","Celeen Rusk","Gary Sperka","Iulia Shoimaru","James Hwang","John Dees","Katy Duncan","Laura Crowe","Lorena Mesa","Matt Higgins","Michael Hamel","Morgan McCrory","Morgan O'Leary","Neal Peters","Sarah Taurchini","Jack Abernethy","Mohammad Amin","Zollie Barnes","Reuben Brandt","Dana Breen","Breton Burnett","Saundra Vanessa Caneda","Luis De Castro","Nicolette Chambers","Kerry Choy","Nick Davies","KB DiAngelo","Katherine DiAngelo","Adrian Diaz","David Diaz","Bob Dorff","Michael Du","Paul Dynowski","Jenna Espezua","Sean Fleming","Jennifer Gilbert","Marcel Haesok","Albert Hahn","Arthur Head","Jonathan Huang","Thomas Huang","Alex Jamar","Kevin Jones","Steven Jones","Cole Kent","Coleby Kent","Caroline","Caroline Kenworthy","Calvin Lang","Yi Lu","David Ma","Sean Massih","Tom McHenry","Alex Mitzman","Lydia Nash","Brenda Nguyen","Matthew Oppenheimer","Mason Pierce","Joe Plonsker","Mira Scarvalone","Joseph Scott","Chris Shahin","Benjamin Shpringer","Lindsey Stevenson","Phil Thomas","Philip Thomas","Gary Tso","Ting Wang","Monique Williamson"]
+people3 = ["Adell Hanson-Kahn","Alex DeLaPena","Bison Hubert","Caitlyn Yu","Caroline Artz","Celeen Rusk","Gary Sperka","Iulia Shoimaru","James Hwang","John Dees","Katy Duncan","Laura Crowe","Lorena Mesa","Matt Higgins","Michael Hamel","Morgan McCrory","Morgan O'Leary","Neal Peters","Sarah Taurchini","Jack Abernethy","Mohammad Amin","Zollie Barnes","Reuben Brandt","Dana Breen","Breton Burnett","Saundra Vanessa Caneda","Luis De Castro","Nicolette Chambers","Kerry Choy","Nick Davies","KB DiAngelo","Katherine DiAngelo","Adrian Diaz","David Diaz","Bob Dorff","Michael Du","Paul Dynowski","Jenna Espezua","Sean Fleming","Jennifer Gilbert","Marcel Haesok","Albert Hahn","Arthur Head","Jonathan Huang","Thomas Huang","Alex Jamar","Kevin Jones","Steven Jones","Cole Kent","Coleby Kent","Caroline","Caroline Kenworthy","Calvin Lang","Yi Lu","David Ma","Sean Massih","Tom McHenry","Alex Mitzman","Lydia Nash","Brenda Nguyen","Matthew Oppenheimer","Mason Pierce","Joe Plonsker","Mira Scarvalone","Joseph Scott","Chris Shahin","Benjamin Shpringer","Lindsey Stevenson","Phil Thomas","Philip Thomas","Gary Tso","Ting Wang"]
+people4 = ["Adell Hanson-Kahn","Alex DeLaPena","Bison Hubert","Caitlyn Yu","Caroline Artz","Celeen Rusk","Gary Sperka","Iulia Shoimaru","James Hwang","John Dees","Katy Duncan","Laura Crowe","Lorena Mesa","Matt Higgins","Michael Hamel","Morgan McCrory","Morgan O'Leary","Neal Peters","Sarah Taurchini","Jack Abernethy","Mohammad Amin","Zollie Barnes","Reuben Brandt","Dana Breen","Breton Burnett","Saundra Vanessa Caneda","Luis De Castro","Nicolette Chambers","Kerry Choy","Nick Davies","KB DiAngelo","Katherine DiAngelo","Adrian Diaz","David Diaz","Bob Dorff","Michael Du","Paul Dynowski","Jenna Espezua","Sean Fleming","Jennifer Gilbert","Marcel Haesok","Albert Hahn","Arthur Head","Jonathan Huang","Thomas Huang","Alex Jamar","Kevin Jones","Steven Jones","Cole Kent","Coleby Kent","Caroline","Caroline Kenworthy","Calvin Lang","Yi Lu","David Ma","Sean Massih","Tom McHenry","Alex Mitzman","Lydia Nash","Brenda Nguyen","Matthew Oppenheimer","Mason Pierce","Joe Plonsker","Mira Scarvalone","Joseph Scott","Chris Shahin","Benjamin Shpringer","Lindsey Stevenson","Phil Thomas","Philip Thomas","Gary Tso"]
+people5 = ["Adell Hanson-Kahn","Alex DeLaPena","Bison Hubert","Caitlyn Yu","Caroline Artz","Celeen Rusk","Gary Sperka","Iulia Shoimaru","James Hwang","John Dees","Katy Duncan","Laura Crowe","Lorena Mesa","Matt Higgins","Michael Hamel","Morgan McCrory","Morgan O'Leary","Neal Peters","Sarah Taurchini","Jack Abernethy","Mohammad Amin","Zollie Barnes","Reuben Brandt","Dana Breen","Breton Burnett","Saundra Vanessa Caneda","Luis De Castro","Nicolette Chambers","Kerry Choy","Nick Davies","KB DiAngelo","Katherine DiAngelo","Adrian Diaz","David Diaz","Bob Dorff","Michael Du","Paul Dynowski","Jenna Espezua","Sean Fleming","Jennifer Gilbert","Marcel Haesok","Albert Hahn","Arthur Head","Jonathan Huang","Thomas Huang","Alex Jamar","Kevin Jones","Steven Jones","Cole Kent","Coleby Kent","Caroline","Caroline Kenworthy","Calvin Lang","Yi Lu","David Ma","Sean Massih","Tom McHenry","Alex Mitzman","Lydia Nash","Brenda Nguyen","Matthew Oppenheimer","Mason Pierce","Joe Plonsker","Mira Scarvalone","Joseph Scott","Chris Shahin","Benjamin Shpringer","Lindsey Stevenson","Phil Thomas","Philip Thomas"]
+
+
+# Pseudocode
+# => Input:  An array
+# => Output:  A grouped array, where each element is a group of 3-5 students
+# => 1. Determine how many people are in the array total (n)
+# => 2. Determine how many are left over once we're done creating groups of 5
+# => 3. The real challenge will actually be the last two groups
+# => 	a. So really, we are interested in (n / 5) + 5
+# =>  	b. If it's 5, then no problem!
+# =>  	c. If it's 6, then we can make a group of 3 and 3
+# =>  	d. If it's 7, then we can make a group of 3 and 4
+# =>  	e. If it's 8, then we can make a group of 4 and 4
+# =>  	f. If it's 9, then we can make a group of 4 and 5
+# =>  	g. If it's 10, then no problem!
+# => 4. Once, we've figured this out, we can group the array into 5's and then the remainder
+
+
+# Initial solution
+def groups(array)
+
+  # Initial grouping creates groups of 5. All groups will be 5, except the last one, which could be 1-5
+  new_array = []
+  i = 0
+  while i < array.length
+    new_array.push(array[i..i+4])
+    i += 5
+  end
+
+  # Balances out the last two groups if the last group is smaller than 3
+  until new_array[new_array.length - 1].length > 2
+    new_array[new_array.length - 1] = new_array[new_array.length - 1].push(new_array[new_array.length - 2].pop)
+  end
+  new_array
+end
+
+# Testing with various group sizes
+p groups(people).map {|i| i.length}
+p groups(people1).map {|i| i.length}
+p groups(people2).map {|i| i.length}
+p groups(people3).map {|i| i.length}
+p groups(people4).map {|i| i.length}
+p groups(people5).map {|i| i.length}
+
+
+# Refactored solution
+
+def groups(array)
+
+  # Slice array into groups of 5	
+  new_array = []
+  array.each_slice(5) {|s| new_array.push(s)}
+  new_array.reverse!
+
+  # Using the array reversed (because it's easier to work with just the first two than last two), balance it out
+  until new_array[0].length > 2
+  	new_array[0].push(new_array[1].pop)
+  end
+  new_array
+end
+
+# Testing with various group sizes
+p groups(people).map {|i| i.length}
+p groups(people1).map {|i| i.length}
+p groups(people2).map {|i| i.length}
+p groups(people3).map {|i| i.length}
+p groups(people4).map {|i| i.length}
+p groups(people5).map {|i| i.length}
+
+# Reflection
+
+# What was the most interesting and most difficult part of this challenge?
+# => The most interesting was figuring out a more elegant way in the refactored version.
+# => I think I still struggle with how local variables are handled, I struggled mostly with when stuff was being outputted, saved, etc.
+# => I thought the logic itself made sense after trying a few cases
+
+# Do you feel you are improving in your ability to write pseudocode and break the problem down?
+# => Yeah, I think so. I think it's still variables that are the hardest. 
+
+# Was your approach for automating this task a good solution? What could have made it even better?
+# => My approach was to group them first
+# => And then deal with the weird cases when groups weren't big enough.
+
+# What data structure did you decide to store the accountability groups in and why?
+# => I think the array here was sufficient, since we didn't necesssarily need to remember the order or anything.
+# => A hash might've been good if we wanted to save the groups with names and call them.
+
+# What did you learn in the process of refactoring your initial solution? Did you learn any new Ruby methods?
+# => I learned how to use .each_slice better.
+# => I think I'm starting to get more comfortable with push and pop as well.
+# => I also like that .map is a good way to create a new array, which I used a lot in testing. 
